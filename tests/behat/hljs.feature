129ec9d208c8120ae45343316bba73c7ed616680
Mark Sharp <m.sharp@chi.ac.uk>
2021-05-15 08:43:51 +0100

    New tests

---
+++ tests/behat/hljs.feature
@@ -0,0 +1,32 @@
+@filter @filter_syntaxhighlighter @uoc
+Feature: Render Code using SyntaxHiglighter filters
+  To highlight code with a SyntaxHiglighter
+
+  Background:
+    Given the following "courses" exist:
+      | shortname | fullname |
+      | C1        | Course 1 |
+    And the following "users" exist:
+      | username | firstname | lastname | email |
+      | teacher1 | Teacher | 1 | teacher1@example.com |
+      | student1 | Student | 1 | student1@example.com |
+    And the following "course enrolments" exist:
+      | user | course | role |
+      | teacher1 | C1 | editingteacher |
+      | student1 | C1 | student        |
+    And the following "activities" exist:
+      | activity | name       | intro      | introformat | course | content   | contentformat | idnumber |
+      | page     | PageName1  | PageDesc1  | 1           | C1     | HLJStest  | 1             | 1        |
+      | page     | PageName2  | PageDesc1  | 1           | C1     | HLJStest  | 1             | 1        |
+    And the "syntaxhighlighter" filter is "on"
+
+    @javascript @external
+    Scenario: Render autodetect language.
+        Given I log in as "teacher1"
+        And I am on "Course 1" course homepage
+        And I follow "PageName1"
+        And I navigate to "Edit settings" in current page administration
+        And I set the field "Page content" to "<p>```</p><p>echo \"Hello\";</p><p>```<br></p>"
+        When I click on "Save and display" "button"
+        And I wait until the page is ready
+        Then ".hljs-keyword" "css_element" should exist
---
+++ tests/filter_test.php
@@ -0,0 +1,97 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Unit Tests for syntaxhighligher
+ *
+ * @package   block_syntaxhighlighter
+ * @author    Mark Sharp <m.sharp@chi.ac.uk>
+ * @copyright 2021 University of Chichester {@link https://www.chi.ac.uk}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+require_once($CFG->dirroot . '/filter/syntaxhighlighter/filter.php'); // Include the code to test.
+
+/**
+ * Filter unit test
+ */
+class filter_test extends advanced_testcase {
+
+    /**
+     * General test filter
+     *
+     * @return void
+     */
+    public function test_autodetect() {
+        $this->resetAfterTest(true);
+
+        $filterplugin = new filter_syntaxhighlighter(null, []);
+
+        $tests = [
+            [
+                'in' => '<p>```</p><p>echo "Hello";</p><p>```<br></p>',
+                'out'=> "<p><pre><code>\necho \"Hello\";\n</code></pre><br></p>"
+            ],
+            [
+                'in' => '<pre>```echo "Hello";```</pre>',
+                'out' => '<pre><code>echo "Hello";</code></pre>'
+            ],
+            [
+                'in' => '<pre><code>echo "Hello";</code></pre>',
+                'out' => '<pre><code>echo "Hello";</code></pre>'
+            ],
+        ];
+
+        foreach ($tests as $test) {
+            $filtered = $filterplugin->filter($test['in']);
+            $this->assertEquals($test['out'], $filtered);
+        }
+    }
+
+    /**
+     * Test specified language.
+     *
+     * @return void
+     */
+    public function test_lang() {
+        $this->resetAfterTest(true);
+
+        $filterplugin = new filter_syntaxhighlighter(null, []);
+
+        $tests = [
+            [
+                'in' => '<p>```lang:php;;</p><p>echo "Hello";</p><p>```<br></p>',
+                'out'=> "<p><pre><code class=\"lang-php\">\necho \"Hello\";\n</code></pre><br></p>"
+            ],
+            [
+                'in' => '<pre>```lang:php;;echo "Hello";```</pre>',
+                'out' => '<pre><code class="lang-php">echo "Hello";</code></pre>'
+            ],
+            [
+                'in' => '<pre><code class="lang-php">echo "Hello";</code></pre>',
+                'out' => '<pre><code class="lang-php">echo "Hello";</code></pre>'
+            ]
+        ];
+
+        foreach ($tests as $test) {
+            $filter = $filterplugin->filter($test['in']);
+            $this->assertContains($test['out'], $filter);
+        }
+    }
+}
